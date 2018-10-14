e104e158b176f2f38fb1e91e02f153c5ec60d7a4
Sierra Gregg <sierragregg@gmail.com>
2018-10-14 06:06:23 -0500

    socket io tweaks

--- client/src/socket/socket_middleware.js
+++ client/src/socket/socket_middleware.js
@@ -3,14 +3,18 @@ import { SENT_CHAT_MSG, GOT_CHAT_MSG } from '../redux/constants';
 import { store } from '../index';
 
 export default function socketMiddleware() {
-  const socket = io.connect('http://localhost:4000');
+  const socket = io.connect(
+    process.env.NODE_ENV === 'development'
+      ? 'http://localhost:5000'
+      : window.location
+  );
 
   socket.on('bot msg', botData => {
     console.log(botData);
     if (botData) {
-      store.dispatch({type: GOT_CHAT_MSG, payload: {msg: botData[0]}});
+      store.dispatch({ type: GOT_CHAT_MSG, payload: { msg: botData[0] } });
       if (botData.length > 1) {
-        store.dispatch({type: GOT_CHAT_MSG, payload: {msg: botData[1]}});
+        store.dispatch({ type: GOT_CHAT_MSG, payload: { msg: botData[1] } });
       }
     }
   });
