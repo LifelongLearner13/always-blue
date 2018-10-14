929ddd21ce804f9e8ebd2de4df9857da223cb1ca
Sebastian Murphy <sebastianmurphy@gmail.com>
2018-10-13 23:55:40 -0500

    cleaned up bot.js

--- bot_response.js
+++ bot_response.js
@@ -4,31 +4,60 @@ module.exports = {
     "I'm always blue, what can I help you with",
     'What can I help you with today',
   ],
-  job: [
-    'What type of job are you looking for?',
-    'What kind of job do you want?',
-    'What kind of job are you good at?',
-  ],
-  labor: [
-    'Are you looking for a day laborer position?',
-    'Are you looking for day laborer work?',
-  ],
-  agricultue: [
-    'Are you looking for a position in agricultrue?',
-    'Were you looking to work on a farm?',
-  ],
-  automotive: [
-    'Are you looking for a mechanic position?',
-    'Do you want to be an automotive mechanic?',
-  ],
-  construction: [
-    'Are you looking for a construction work?',
-    'Do you to be a construction worker?',
-  ],
-  housekeeping: [
-    'Are you looking for a position as a maid?',
-    'Are you looking for work cleaning?',
-  ],
+  job: {
+    job: [
+      'What type of job are you looking for?',
+      'What kind of job do you want?',
+      'What kind of job are you good at?',
+    ],
+    job_spanish: [
+      '¿Qué tipo de trabajo estás buscando?',
+      '¿Qué clase de trabajo quieres?',
+      '¿En qué tipo de trabajo eres bueno?',
+    ],
+  },
+  job_category: {
+    labor: [
+      'Are you looking for a day laborer position?',
+      'Are you looking for day laborer work?',
+    ],
+    agricultue: [
+      'Are you looking for a position in agricultrue?',
+      'Were you looking to work on a farm?',
+    ],
+    automotive: [
+      'Are you looking for a mechanic position?',
+      'Do you want to be an automotive mechanic?',
+    ],
+    construction: [
+      'Are you looking for a construction work?',
+      'Do you to be a construction worker?',
+    ],
+    housekeeping: [
+      'Are you looking for a position as a maid?',
+      'Are you looking for work cleaning?',
+    ],
+    labor_spanish: [
+      '¿Estás buscando un puesto de jornalero?',
+      '¿Buscas trabajo de jornalero?',
+    ],
+    agricultue_spanish: [
+      '¿Buscas un puesto en la agricultura?',
+      '¿Quieres trabajar en una granja?',
+    ],
+    automotive_spanish: [
+      '¿Buscas trabajo como mecánico?',
+      '¿Quieres ser mecánico de automoción?',
+    ],
+    construction_spanish: [
+      '¿Buscas un trabajo de construcción?',
+      '¿Quieres ser un trabajador de la construcción?',
+    ],
+    housekeeping_spanish: [
+      '¿Estás buscando una posición como sirvienta?',
+      '¿Buscas trabajo de limpieza?',
+    ],
+  },
   none: [
     'Is there something else that I can help you with?',
     'Is there anything else that I can do for you?',
@@ -38,31 +67,7 @@ module.exports = {
     'Siempre soy azul, ¿en qué puedo ayudarte?',
     '¿Con que puedo ayudarte hoy?',
   ],
-  job_spanish: [
-    '¿Qué tipo de trabajo estás buscando?',
-    '¿Qué clase de trabajo quieres?',
-    '¿En qué tipo de trabajo eres bueno?',
-  ],
-  labor_spanish: [
-    '¿Estás buscando un puesto de jornalero?',
-    '¿Buscas trabajo de jornalero?',
-  ],
-  agricultue_spanish: [
-    '¿Buscas un puesto en la agricultura?',
-    '¿Quieres trabajar en una granja?',
-  ],
-  automotive_spanish: [
-    '¿Buscas trabajo como mecánico?',
-    '¿Quieres ser mecánico de automoción?',
-  ],
-  construction_spanish: [
-    '¿Buscas un trabajo de construcción?',
-    '¿Quieres ser un trabajador de la construcción?',
-  ],
-  housekeeping_spanish: [
-    '¿Estás buscando una posición como sirvienta?',
-    '¿Buscas trabajo de limpieza?',
-  ],
+
   none_spanish: [
     '¿Hay algo más con lo que pueda ayudarte?',
     '¿Hay algo más que pueda hacer por ti?',
