31a5664d92d9fe3fd00abf7829da301500f80f07
jediditty <jediditty@gmail.com>
2018-10-14 07:52:04 -0500

    stuff

--- client/src/socket/socket_middleware.js
+++ client/src/socket/socket_middleware.js
@@ -1,21 +1,19 @@
-import io from 'socket.io-client';
 import { SENT_CHAT_MSG, GOT_CHAT_MSG } from '../redux/constants';
 import { store } from '../index';
+import io from 'socket.io-client';
+const socket = io();
+socket.on('bot msg', botData => {
+  console.log(botData);
+  if (botData) {
+    store.dispatch({ type: GOT_CHAT_MSG, payload: { msg: botData[0] } });
+    if (botData.length > 1) {
+      store.dispatch({ type: GOT_CHAT_MSG, payload: { msg: botData[1] } });
+    }
+  }
+});
 
 export default function socketMiddleware() {
-  const socket = io();
-
-  socket.on('bot msg', botData => {
-    console.log(botData);
-    if (botData) {
-      store.dispatch({ type: GOT_CHAT_MSG, payload: { msg: botData[0] } });
-      if (botData.length > 1) {
-        store.dispatch({ type: GOT_CHAT_MSG, payload: { msg: botData[1] } });
-      }
-    }
-  });
-
-  return ({ dispatch }) => next => action => {
+    return ({ dispatch }) => next => action => {
     if (typeof action === 'function') {
       return next(action);
     }
